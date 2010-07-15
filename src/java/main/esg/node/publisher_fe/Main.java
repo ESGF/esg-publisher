95a3814d170ce050f3076ede4682e6dc87c7220b
Carla Hardy <hardy21@llnl.gov>
2010-07-15 14:55:11 -0700

    This commit includes split panels, progress  bar, and expandable menu

--- src/java/main/esg/node/publisher_fe/Main.java
+++ src/java/main/esg/node/publisher_fe/Main.java
@@ -1,3 +1,8 @@
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.impl.*;
 
 
 /**
@@ -6,7 +11,20 @@
  */
 public class Main {
 		public static void main(String[] args) {
-		new Main();
+			try {
+	             UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+	         } catch (UnsupportedLookAndFeelException ex) {
+	             ex.printStackTrace();
+	         } catch (IllegalAccessException ex) {
+	             ex.printStackTrace();
+	         } catch (InstantiationException ex) {
+	             ex.printStackTrace();
+	         } catch (ClassNotFoundException ex) {
+	             ex.printStackTrace();
+	         }
+	         /* Turn off metal's use of bold fonts */
+	         //UIManager.put("swing.boldMetal", Boolean.FALSE);
+				
 		ESGPublisher3 esgDraft = new ESGPublisher3();
 		esgDraft.buildapp();
 		System.out.println("done");
