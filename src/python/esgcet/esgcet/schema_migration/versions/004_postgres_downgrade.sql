8a560f8f3d51e1ccfef9ccb02e4ec7b98ec1ec1d
drach1 <drach1@llnl.gov>
2012-08-21 13:44:06 -0700

    - esgquery_index returns wget scripts - esgquery_index bug related to 'score' fixed - db schema V4 adds indexes on foreign keys to speed up esgunpublish

--- src/python/esgcet/esgcet/schema_migration/versions/004_postgres_downgrade.sql
+++ src/python/esgcet/esgcet/schema_migration/versions/004_postgres_downgrade.sql
@@ -3,5 +3,6 @@
 --
 
 DROP INDEX file_variable_variable_id_index;
+DROP INDEX file_variable_file_id_index;
 DROP INDEX dataset_file_version_id_index;
 DROP INDEX file_version_file_id_index;
--- src/python/esgcet/esgcet/schema_migration/versions/004_postgresql_upgrade.sql
+++ src/python/esgcet/esgcet/schema_migration/versions/004_postgresql_upgrade.sql
@@ -3,6 +3,7 @@
 --
 
 CREATE INDEX file_variable_variable_id_index ON file_variable USING btree (variable_id);
+CREATE INDEX file_variable_file_id_index on file_variable USING btree (file_id);
 ANALYZE file_variable;
 
 CREATE INDEX dataset_file_version_id_index ON dataset_file_version USING btree (file_version_id);
