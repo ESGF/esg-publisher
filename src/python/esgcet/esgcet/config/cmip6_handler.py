cc5f6f0fc729b7ae09ca7db04ef1f628d04ecc5f
Alan Iwi <alan.iwi@stfc.ac.uk>
2017-05-26 15:09:06 +0100

    remove createCim added to cmip6_handler.py; it does not live there. Corresponding config option will need added in "config" repo

--- src/python/esgcet/esgcet/config/cmip6_handler.py
+++ src/python/esgcet/esgcet/config/cmip6_handler.py
@@ -34,8 +34,6 @@ class CMIP6Handler(BasicHandler):
         BasicHandler.__init__(self, name, path, Session, validate=validate, offline=offline)
 
 
-    createCim = True
-
 
     def openPath(self, path):
         """Open a sample path, returning a project-specific file object,
