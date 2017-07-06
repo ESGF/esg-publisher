c1b19419e462d48931f55487d6f2d79a9a76c460
Sasha Ames <amysash2006@gmail.com>
2017-07-06 14:44:59 -0700

    update esgprep

--- src/python/esgcet/setup.py
+++ src/python/esgcet/setup.py
@@ -69,7 +69,7 @@ setup(
         "lxml>=2.0",
         "sqlalchemy_migrate>=0.6",
         "requests>=1.2.0",
-        "esgprep>=2.7.5",
+        "esgprep>=2.7.7",
         "MyProxyClient>=2.0.1",
         "esgf-pyclient>=0.1.8",
         "esgfpid>=0.7.8",
