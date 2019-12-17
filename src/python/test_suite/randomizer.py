14e31b5db56acddea0fe537b3e6a2768afbad16e
sashakames <ames4@llnl.gov>
2019-12-17 15:56:23 -0800

    add consts

--- src/python/test_suite/randomizer.py
+++ src/python/test_suite/randomizer.py
@@ -1,23 +1,26 @@
 import sys, os
 from random import sample
 
+NUM_BATCHES = 100
+NUM_PER_BATCH = 100
+
 def get_rand_lines(infile, count):
 
     return sample([line for line in infile], count)
 
 
 
-arr = get_rand_lines(open(sys.argv[1]), 10000)
+arr = get_rand_lines(open(sys.argv[1]), NUM_BATCHES * NUM_PER_BATCH)
 
 
-for i in range(100):
+for i in range(NUM_BATCHES):
 
 
     outf=open("map_batch_{}.lst".format(i+1))
 
-    for j in range(100):
+    for j in range(NUM_PER_BATCH):
 
-        outf.write(arr[i*100+j])
+        outf.write(arr[i*NUM_PER_BATCH+j])
     outf.close()
 
 
