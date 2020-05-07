11e6347b65278cd2a990f046096282c770d77528
Sasha Ames <amysash2006@gmail.com>
2020-05-07 13:00:07 -0700

    inital script

---
+++ install-publisher.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# recommended shebang....
+
+#CONDA_HOME=$HOME/conda
+CONDA_HOME=/usr/local/conda
+
+
+check_error() {
+if [ $? != 0 ] ; then
+    
+    echo Error has occurred.  Exiting install.  Please report this error to the ESGF development team.
+    exit -1
+fi
+}
+
+if [ -d  $CONDA_HOME ] ; then
+
+    source $CONDA_HOME/bin/activate
+else
+    # try if in path already
+    source activate
+fi
+check_error
+conda create -n pub-py3 -c conda-forge cmor cdms2 pip git
+check_error
+conda activate pub-py3
+check_error
+pip install 'git+https://github.com/IS-ENES-Data/esgf-pid@python_3.7' 'git+https://github.com/sashakames/esdoc-cdf2cim.git' 'git+https://github.com/ESGF/esg-publisher@python3#subdirectory=src/python/esgcet'
+check_error
+ 
+
+
+
+
