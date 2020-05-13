b555e03829086ea651ecc48ae31fe44b661defeb
Katharina Berger <berger@dkrz.de>
2020-05-13 16:46:47 +0200

    fix schema migration change

--- src/python/esgcet/esgcet/schema_migration/versions/009_postgres_downgrade.sql
+++ src/python/esgcet/esgcet/schema_migration/versions/009_postgres_downgrade.sql
@@ -2,7 +2,7 @@
 -- Revert location size changes
 --
 
-update catalog set location='value_too_long' length(location)>255;
+update catalog set location='value_too_long' where length(location)>255;
 
 ALTER TABLE catalog
   ALTER location TYPE CHARACTER VARYING(255);
