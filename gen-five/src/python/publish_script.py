83b8e523700bd26079438dbcbe9d9de1c6a45445
Elysia Witham <ewitham@pop-os.localdomain>
2020-06-22 14:30:45 -0700

    Modifications to publish_script

--- gen-five/src/python/publish_script.py
+++ gen-five/src/python/publish_script.py
@@ -7,13 +7,37 @@ import os
 import json
 import sys
 import tempfile
+import cmip6_handler as prep
+
+
+def prepare(fm_file):
+    save = True
+    print("Iterating through filenames for PrePARE...")
+    for line in fm_file:  # iterate through mapfile files for PrePARE -- import PrePARE and run natively
+        # see cmip6_handler.py for help
+        split_line = line.split(" | ")
+        filename = split_line[2]
+        if save:
+            path = filename
+            save = False
+        # command = "PrePARE --table-path " + cmor_tables + " " + filename
+        # os.system(command)  # subprocess module?? change this perhaps
+        prep.validateFile(filename)
+
+    return path
+
+def exit_cleanup():
+    scan_file.close()
+    fullmap_file.close()
 
 
 if len(sys.argv) < 1:
     print("Usage: python3 publish_script.py </path/to/mapfile>")
 
 fullmap = sys.argv[1]  # full mapfile path
+# allow handling of multiple mapfiles later
 proj = "CMIP6"  # needed for mapfile.py
+# if not cmip6 PrePARE and pid aren't necessary -- take proj as argument in final product
 basename = str(os.path.basename(fullmap))  # base dir of full map directory
 
 scan_file = tempfile.NamedTemporaryFile()  # create a temporary file which is deleted afterward for autocurator
@@ -29,25 +53,24 @@ os.system("cert_path=./cert.pem")  # TODO: fix this
 os.system("export LD_LIBRARY_PATH=$CONDA_PREFIX/lib")  # this isn't working for some reason ...
 
 fullmap_file = open(fullmap, 'r')  # open file object for PrePARE
-path = ""
-save = True
-print("Iterating through filenames for PrePARE and Autocurator...")
-for line in fullmap_file:  # iterate through mapfile files for PrePARE
-    split_line = line.split(" | ")
-    filename = split_line[2]
-    if save:
-        path = filename
-        save = False
-    command = "PrePARE --table-path " + cmor_tables + " " + filename
-    os.system(command)
-    os.system(autoc_command + " --files " + filename)
-fullmap_file.close()
+try:
+    path = prepare(fullmap_file)
+except Exception as ex:
+    print("Error with PrePARE: " + ex)
+    exit_cleanup()
+    exit(1)
+
 
 datasetdir = os.path.dirname(path) + "/*.nc"  # TODO: fix -- autocurator having issues with *
 # temporary fix:
 dataset_test = "/p/css03/esgf_publish/CMIP6/CMIP/NCAR/CESM2/historical/r2i1p1f1/Emon/cTotFireLut/gn/v20190308/cTotFireLut_Emon_CESM2_historical_r2i1p1f1_gn_185001-201412.nc"
 
 # Run autocurator and all python scripts
+print("Done.\nRunning autocurator...")
+os.system(autoc_command + " --files " + dataset_test)
+
+# print("Printing contents of scanfn")
+# os.system("cat " + scanfn)
 
 print("Done.\nConverting mapfile...")
 map_json_data = mp.main([fullmap, proj])
@@ -65,4 +88,4 @@ print("Done.\nRunning pub test...")
 pt.main(out_json_data)
 
 print("Done. Cleaning up.")
-scan_file.close()
+exit_cleanup()
