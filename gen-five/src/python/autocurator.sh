ddacafe550c3aea8a18335f25989cd562d469884
Elysia Witham <ewitham@pop-os.localdomain>
2020-06-23 09:58:30 -0700

    Found work around for autocurator/PrePARE

--- gen-five/src/python/autocurator.sh
+++ gen-five/src/python/autocurator.sh
@@ -1,7 +1,7 @@
 
+autocur_cmd=$1
 fullmap=$2
 scanfn=$3
 path=`head -n1 $fullmap | awk '{print $3}'`
 datasetdir=`dirname ${path}`/'*.nc'
-autocur_cmd=$1
-$autocur_cmd --out_json $scanfn --files "$datasetdir"
+$autocur_cmd --out_pretty --out_json $scanfn --files "$datasetdir"
--- gen-five/src/python/publish_script.py
+++ gen-five/src/python/publish_script.py
@@ -13,82 +13,102 @@ import subprocess
 # TODO: running into file dependency errors with internal representation of PrePARE
 
 
-def prepare(fm_file):
-    save = True
+def prepare(fm_file, cmor_tables):
     print("Iterating through filenames for PrePARE...")
     for line in fm_file:  # iterate through mapfile files for PrePARE -- import PrePARE and run natively
-        # see cmip6_handler.py for help
+        # see cmip6_handler.py for help -- error with file dependencies
         split_line = line.split(" | ")
         filename = split_line[2]
-        if save:
-            path = filename
-            save = False
         command = "PrePARE --table-path " + cmor_tables + " " + filename
         os.system(command)  # subprocess module?? change this perhaps
         # issue with subprocess: output not as easily shown
         # prep.validateFile(fm_file)
-    return path
-
+    print("Done.")
 
 def exit_cleanup():
     scan_file.close()
     fullmap_file.close()
 
 
-if len(sys.argv) < 1:
-    print("Usage: python3 publish_script.py </path/to/mapfile>")
+def main(args):
+    if len(args) < 2:
+        print("Usage: python3 publish_script.py </path/to/mapfile> <project>")
 
-os.system("export LD_LIBRARY_PATH=$CONDA_PREFIX/lib")  # this isn't working for some reason ...
+    os.system("export LD_LIBRARY_PATH=$CONDA_PREFIX/lib")  # this isn't working for some reason ...
 
-fullmap = sys.argv[1]  # full mapfile path
-# allow handling of multiple mapfiles later
-proj = "CMIP6"  # needed for mapfile.py
-# if not cmip6 PrePARE and pid aren't necessary -- take proj as argument in final product
-basename = str(os.path.basename(fullmap))  # base dir of full map directory
+    fullmap = args[0]  # full mapfile path
+    # allow handling of multiple mapfiles later
 
-scan_file = tempfile.NamedTemporaryFile()  # create a temporary file which is deleted afterward for autocurator
-scanfn = scan_file.name  # name to refer to tmp file
+    proj = args[1]
+    cmip6 = False
+    if proj == "CMIP6":
+        cmip6 = True
 
-cmor_tables = input("Path to cmor tables: ")  # interactive script, should require no internal editing
-autocurator = input("Path to autocurator: ")  # so we just get variable paths from user
+    scan_file = tempfile.NamedTemporaryFile()  # create a temporary file which is deleted afterward for autocurator
+    scanfn = scan_file.name  # name to refer to tmp file
 
+    cmor_tables = input("Path to cmor tables: ")  # interactive script, should require no internal editing
+    autocurator = input("Path to autocurator: ")  # so we just get variable paths from user
 
-autoc_command = autocurator + "/bin/autocurator --out_pretty"  # concatenate autocurator command
+    autoc_command = autocurator + "/bin/autocurator"  # concatenate autocurator command
 
-os.system("cert_path=./cert.pem")  # TODO: fix this
+    os.system("cert_path=./cert.pem")  # TODO: fix this
 
-fullmap_file = open(fullmap, 'r+')  # open file object for PrePARE
-try:
-    path = prepare(fullmap_file)
-except Exception as ex:
-    print("Error with PrePARE: " + ex)
+    fullmap_file = open(fullmap, 'r')  # open file object for PrePARE
+    if cmip6:
+        try:
+            prepare(fullmap_file, cmor_tables)
+        except Exception as ex:
+            print("Error with PrePARE: " + ex)
+            exit_cleanup()
+            exit(1)
+
+    # Run autocurator and all python scripts
+    print("Running autocurator...")
+    os.system("bash gen-five/src/python/autocurator.sh " + autoc_command + " " + fullmap + " " + scanfn)
+
+    print("Done.\nConverting mapfile...")
+    try:
+        map_json_data = mp.main([fullmap, proj])
+    except Exception as ex:
+        print("Error with converting mapfile: " + ex)
+        exit_cleanup()
+        exit(1)
+
+    print("Done.\nMaking dataset...")
+    try:
+        out_json_data = mkd.main([map_json_data, scanfn])
+    except Exception as ex:
+        print("Error making dataset: " + ex)
+        exit_cleanup()
+        exit(1)
+
+    if cmip6:
+        print("Done.\nRunning pid cite...")
+        try:
+            out_json_data = pid.main(out_json_data)
+        except Exception as ex:
+            print("Error running pid cite: " + ex)
+
+    print("Done.\nUpdating...")
+    try:
+        up.main(out_json_data)
+    except Exception as ex:
+        print("Error updating: " + ex)
+        exit_cleanup()
+        exit(1)
+
+    print("Done.\nRunning pub test...")
+    try:
+        pt.main(out_json_data)
+    except Exception as ex:
+        print("Error running pub test: " + ex)
+        exit_cleanup()
+        exit(1)
+
+    print("Done. Cleaning up.")
     exit_cleanup()
-    exit(1)
 
 
-datasetdir = os.path.dirname(path) + "/*.nc"  # TODO: fix -- autocurator having issues with *
-# temporary fix:
-dataset_test = "/p/css03/esgf_publish/CMIP6/CMIP/NCAR/CESM2/historical/r2i1p1f1/Emon/cTotFireLut/gn/v20190308/cTotFireLut_Emon_CESM2_historical_r2i1p1f1_gn_185001-201412.nc"
-
-# Run autocurator and all python scripts
-print("Done.\nRunning autocurator...")
-os.system("bash gen-five/src/python/autocurator.sh " + autoc_command + " " + fullmap + " " + scanfn)
-os.system('cat ' + scanfn)
-
-print("Done.\nConverting mapfile...")
-map_json_data = mp.main([fullmap, proj])
-
-print("Done.\nMaking dataset...")
-out_json_data = mkd.main([map_json_data, scanfn])
-
-print("Done.\nRunning pid cite...")
-out_json_data = pid.main(out_json_data)
-
-print("Done.\nUpdating...")
-up.main(out_json_data)
-
-print("Done.\nRunning pub test...")
-pt.main(out_json_data)
-
-print("Done. Cleaning up.")
-exit_cleanup()
+if __name__ == '__main__':
+    main(sys.argv)
