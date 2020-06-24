f4c47fa087122a5c48122d1391025f37b1e41378
Elysia Witham <ewitham@pop-os.localdomain>
2020-06-24 15:05:20 -0700

    Edits to fix prepare

--- gen-five/src/python/publish_script.py
+++ gen-five/src/python/publish_script.py
@@ -9,23 +9,17 @@ import sys
 import tempfile
 import subprocess
 from cmip6_cv import PrePARE
-# from my-publisher.gen-five.src.esgcet.config import cmip6_handler as prep
-# import cmip6_handler as prep
-# TODO: running into file dependency errors with internal representation of PrePARE
 
 
-def prepare_internal(fm_file, cmor_tables):
+def prepare_internal(json_map, cmor_tables):
     print("iterating through filenames for PrePARE (internal version)...")
     validator = PrePARE.PrePARE
-    path = fm_file.path
-    for line in fm_file:
-        split_line = line.split(" | ")
-        filename = split_line[2]
+    for info in json_map:
+        filename = info[1]
         # file_cmor_version = fm_file.getAttribute('cmor_version', None) skipping for now
         # data_specs_version = fm_file.getAttribute('data_specs_version', None) might not be necessary
         process = validator.checkCMIP6(cmor_tables)
-        process.ControlVocab(path)
-
+        process.ControlVocab(filename)
 
 
 def prepare(fm_file, cmor_tables):
@@ -33,12 +27,13 @@ def prepare(fm_file, cmor_tables):
     for line in fm_file:  # iterate through mapfile files for PrePARE -- import PrePARE and run natively
         # see cmip6_handler.py for help -- error with file dependencies
         split_line = line.split(" | ")
-        filename = split_line[2]
+        filename = split_line[1]
         command = "PrePARE --table-path " + cmor_tables + " " + filename
         os.system(command)  # subprocess module?? change this perhaps
         # issue with subprocess: output not as easily shown
         # prep.validateFile(fm_file)
     print("Done.")
+
 
 def exit_cleanup(scan_file, fullmap_file):
     scan_file.close()
@@ -46,16 +41,15 @@ def exit_cleanup(scan_file, fullmap_file):
 
 
 def main(args):
-    if len(args) < 2:
-        print("Usage: python3 publish_script.py </path/to/mapfile> <project>")
 
-    os.system("export LD_LIBRARY_PATH=$CONDA_PREFIX/lib")  # this isn't working for some reason ...
-
-    fullmap = args[1]  # full mapfile path
-    # allow handling of multiple mapfiles later
-
-    proj = "CMIP6"
-    cmip6 = True
+    fullmap = args[2]  # full mapfile path
+    split_map = fullmap.split("/")
+    fname = split_map[-1]
+    fname_split = fname.split(".")
+    proj = fname_split[0]
+    cmip6 = False
+    if proj == "CMIP6":
+        cmip6 = True
 
     scan_file = tempfile.NamedTemporaryFile()  # create a temporary file which is deleted afterward for autocurator
     scanfn = scan_file.name  # name to refer to tmp file
@@ -67,10 +61,19 @@ def main(args):
 
     os.system("cert_path=./cert.pem")  # TODO: fix this
 
+    print("Converting mapfile...")
+    try:
+        map_json_data = mp.main([fullmap, proj])
+    except Exception as ex:
+        print("Error with converting mapfile: " + str(ex))
+        exit_cleanup(scan_file, fullmap_file)
+        exit(1)
     fullmap_file = open(fullmap, 'r')  # open file object for PrePARE
+    print("Done.")
+
     if cmip6:
         try:
-            prepare(fullmap_file, cmor_tables)
+            prepare(map_json_data, cmor_tables)
         except Exception as ex:
             print("Error with PrePARE: " + str(ex))
             exit_cleanup(scan_file, fullmap_file)
@@ -79,14 +82,6 @@ def main(args):
     # Run autocurator and all python scripts
     print("Running autocurator...")
     os.system("bash gen-five/src/python/autocurator.sh " + autoc_command + " " + fullmap + " " + scanfn)
-
-    print("Done.\nConverting mapfile...")
-    try:
-        map_json_data = mp.main([fullmap, proj])
-    except Exception as ex:
-        print("Error with converting mapfile: " + str(ex))
-        exit_cleanup(scan_file, fullmap_file)
-        exit(1)
 
     print("Done.\nMaking dataset...")
     try:
@@ -125,4 +120,16 @@ def main(args):
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    args = sys.argv
+    if len(args) < 3 or args[1] != "--map":
+        print("Usage: python3 publish_script.py --map </path/to/mapfile>")
+
+    # os.system("export LD_LIBRARY_PATH=$CONDA_PREFIX/lib")  # this isn't working for some reason ...
+
+    fullmap = args[2]  # full mapfile path
+    # allow handling of multiple mapfiles later
+    if fullmap[-1] == "/":
+        pass
+        # iterate through file in directory calling mai
+    else:
+        main(args)
