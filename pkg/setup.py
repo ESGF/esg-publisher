4cf1c0cc56976047ef1dc6f4be9ac1276e761abf
Sasha Ames <amysash2006@gmail.com>
2021-03-22 10:45:00 -0700

    revert to alpha

--- pkg/setup.py
+++ pkg/setup.py
@@ -8,7 +8,7 @@ import configparser as cfg
 from shutil import copyfile
 
 
-VERSION = '5.1.0b1'
+VERSION = '5.1.0a0'
 print("esgcet version =", VERSION)
 HOME = str(Path.home())
 FULLPATH = HOME + '/.esg'
