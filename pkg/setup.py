a0c5a26558d731abce5b4a0e96941be0be67a88c
Sasha Ames <amysash2006@gmail.com>
2021-03-22 10:35:09 -0700

    update version num for testing

--- pkg/setup.py
+++ pkg/setup.py
@@ -8,7 +8,7 @@ import configparser as cfg
 from shutil import copyfile
 
 
-VERSION = '5.0.0a7'
+VERSION = '5.1.0b1'
 print("esgcet version =", VERSION)
 HOME = str(Path.home())
 FULLPATH = HOME + '/.esg'
