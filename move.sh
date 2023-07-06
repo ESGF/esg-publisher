cc4140cff6f8727ce0331d4b100a6c7e6190530e
Sasha Ames <amysash2006@gmail.com>
2023-07-06 09:09:12 -0700

    remove

--- move.sh
+++
@@ -1,4 +0,0 @@
-for file in $(ls /p/user_pub/publish-queue/CMIP6-maps-todo2 | head -n 10000)
-do
-  mv /p/user_pub/publish-queue/CMIP6-maps-todo2/$file /p/user_pub/publish-queue/CMIP6-maps-todo
-done
