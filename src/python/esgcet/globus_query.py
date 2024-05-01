e01fe3f4736c88e9b911dcc9a2cbf38c3be857bd
ames4 <ames4@llnl.gov>
2024-05-01 15:42:20 -0700

    remove extra run

---
+++ src/python/esgcet/globus_query.py
@@ -0,0 +1,88 @@
+import json, copy
+import tempfile
+import sys, os
+
+from subprocess import Popen, PIPE
+
+FILTER_TEMPLATE = {
+    "type" : "match_all",
+    "field_name" : "data_node",
+    "values" : []
+    }
+
+SEARCH_TEMPLATE = {
+    "q" : "",
+    "filters" : [
+        {
+        "type" : "match_all",
+        "field_name" : "latest",
+        "values" : ["true"]
+        }
+        ],
+    }
+
+class ESGGlobusQuery():
+
+    def __init__(self, UUID_in, data_node_in):
+        self._UUID = UUID_in
+        self._data_node_filter = self._add_filter("data_node", data_node_in)
+
+    def _add_filter(self, name, value):
+        tmpfilter = copy.deepcopy(FILTER_TEMPLATE)
+        tmpfilter["field_name"] = name
+        tmpfilter["values"].append(value)
+        return tmpfilter
+    
+    def globus_get_record(self, subj):
+        proc = Popen(["globus", "search", "subject", "show", self._UUID, subj.rstrip()], stdout=PIPE)
+#        cmdstr = f"globus search subject show {self._UUID} '{subj.strip()}'"
+#        proc = Popen(cmdstr            , shell=True, stdout=PIPE)
+ #       print(f"DEBUG {cmdstr}")
+
+ #       os.system(cmdstr)
+        proc.wait()
+#        res = proc.stdout.read().decode()
+#        print(f"DEBUG {res}")
+        return json.load(proc.stdout)
+
+    
+
+    def dataset_query(self, master_id):
+
+        q = copy.deepcopy(SEARCH_TEMPLATE)
+        q["filters"].append(self._data_node_filter)
+        q["filters"].append(self._add_filter("type", "Dataset"))
+        q["filters"].append(self._add_filter("master_id", master_id))
+
+
+        temp = tempfile.NamedTemporaryFile(mode="w", delete=False)
+        json.dump(q, temp, indent=1)
+        print(f"DEBUG: to {temp.name}")
+        temp.close()
+
+        subproc = Popen(["globus", "search", "query", self._UUID, "--query-document",temp.name], stdout=PIPE)
+
+        subproc.wait()
+        subj = ""
+        record = None
+        for x in subproc.stdout:
+            assert not subj  # There should be only one latest, TODO convert to raise an Exception
+            subj = x.decode()
+        
+        if subj:
+            res = self.globus_get_record(subj)
+            if res and "content" in res:
+                return res["content"]
+            else:
+                raise RuntimeError(f"Unexpected error {res}")
+        else:
+            print("No record found")
+            return {}
+        
+# UUID = "5cc79324-1b74-4a77-abc3-838aba2fc734"
+# DATA_NODE = "esgf-fake-test.llnl.gov"
+
+# x = ESGGlobusQuery(UUID, DATA_NODE)
+
+# res = x.dataset_query(sys.argv[1])
+# print(len(res))
---
+++ src/python/esgcet/update_globus.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+
+class ESGUpdateBase:
+
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def update_file(self, dsetid : str):
+        pass
+
+    @abstractmethod
+    def update_dataset(self, dsetid : str):
+        pass
+
+    @abstractmethod
+    def query_update(self):
+        pass
+
+    def run(self, input_rec):
+        """ Check a record in the index and peform the updates
+
+            input_rec - a json record to be published containing a "master_id" and "data_node" fields with 
+                        a new version
+        """
+    # The dataset record either first or last in the input file
+        dset_idx = -1
+        if not input_rec[dset_idx]['type'] == 'Dataset':
+            dset_idx = 0
+
+        if not input_rec[dset_idx]['type'] == 'Dataset':
+            self.publog.error("Could not find the Dataset record.  Malformed input, exiting!")
+            exit(1)
+
+        mst = input_rec[dset_idx]['master_id']
+        dnode = input_rec[dset_idx]['data_node']
+
+        dsetid = self.query_update(dnode, mst)
+
+        if dsetid:
+            self.update_core(dsetid,"datasets")
+            self.update_core(dsetid, "files")
+            self.publog.info('Found previous version, updating the record: {}'.format(dsetid))
+
+        else:
+            version = input_rec[dset_idx]['version']
+            self.publog.info('First dataset version for {}: v{}.)'.format(mst, version))
+
+    def run(self)
