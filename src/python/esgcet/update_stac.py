369571ed0b4492143d7b109ba591646e71089837
rhysrevans3 <rhys.r.evans3@gmail.com>
2026-03-17 14:05:37 +0000

    debugging.

--- src/python/esgcet/update_stac.py
+++ src/python/esgcet/update_stac.py
@@ -1,86 +1,81 @@
 from abc import ABC, abstractmethod
+
 import esgcet.logger as logger
 
 log = logger.ESGPubLogger()
 
-from pystac_client import Client
 from esgcet.stac_client import getTransactionClient
 from esgcet.update_base import ESGUpdateBase
+from pystac_client import Client
 
 FIELDNAME = "base_id"
 
 
 class ESGUpdateSTAC(ESGUpdateBase):
 
-
     def __init__(self, config, silent=False, verbose=False, dry_run=False):
         ESGUpdateBase.__init__(self, silent=silent, verbose=verbose)
 
-        self.pystac_client = Client(config.get("stac_api"), '')
+        self.pystac_client = Client.open(
+            config.get("stac_transaction_api").get("base_url"), ""
+        )
         self._dry_run = dry_run
-        self.trans_client = getTransactionClient(config.get("stac_config",{}))(config)
+        self.trans_client = getTransactionClient(config.get("stac_config", {}))
 
-
-    def update_file(self, dsetid : str):
+    def update_file(self, dsetid: str):
         """
-        Irrelevant 
+        Irrelevant
         """
         pass
 
-    def update_dataset(self, dsetid : str, update_dict={}, set_latest=False):
-
+    def update_dataset(self, dsetid: str, update_dict={}, set_latest=False):
 
         operations = []
-        
-        if not (set_latest):
-            op =                     {
-                        "op": "replace",
-                        "path": "/properties/latest",
-                        "value": set_latest
-                    }
-            operations.append(op) 
 
-                
+        if not (set_latest):
+            op = {"op": "replace", "path": "/properties/latest", "value": set_latest}
+            operations.append(op)
+
         # Use STAC property in update not legacy field names from Solr-era
         if update_dict:
             for k in update_dict:
-    #                props[k] = update_dict[k]
-                op =                     {
-                        "op": "replace",
-                        "path": "/properties/{k}",
-                        "value": update_dict[k]
-                    }
-                operations.append(op) 
+                #                props[k] = update_dict[k]
+                op = {
+                    "op": "replace",
+                    "path": "/properties/{k}",
+                    "value": update_dict[k],
+                }
+                operations.append(op)
 
-        
-        entry = operations  
-        #{ "operations": operations}
+        entry = operations
+        # { "operations": operations}
 
         print(f"DEBUG {self.collection} {dsetid} {entry}")
         collection = self.collection
         response = self.trans_client.json_patch(collection, dsetid, entry)
 
+    def query_update(self, data_node: str, master_id: str):
 
-    def query_update(self, data_node : str, master_id : str):
-
-        parts = master_id.split('.')
+        parts = master_id.split(".")
         if parts[0] == "MIP-DRS7":
             collection = parts[1]
         else:
             collection = parts[0]
-            
-        filt=  { 
-                "op": "and",
-                "args": [
-                {
-                    "op": "=", "args": [{"property": f"properties.{FIELDNAME}"}, master_id]
-                },
-                {
-                    "op": "=", "args": [{"property": "properties.latest"}, True]} ]
 
-                }
-                
-        resp = self.pystac_client.search(collections=[collection],filter=filt, max_items=1)
+        filt = {
+            "op": "and",
+            "args": [
+                {
+                    "op": "=",
+                    "args": [{"property": f"properties.{FIELDNAME}"}, master_id],
+                },
+                {"op": "=", "args": [{"property": "properties.latest"}, True]},
+            ],
+        }
+
+        resp = self.pystac_client.search(
+            collections=[collection], filter=filt, max_items=1
+        )
 
         if resp.matched() < 1:
             return False
@@ -90,9 +85,3 @@ class ESGUpdateSTAC(ESGUpdateBase):
         self.stac_item = d
 
         return d["id"]
-        
-        
-        
-
-
-
