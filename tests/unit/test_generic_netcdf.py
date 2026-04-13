06eacb4bdf1d54d01f232be01fefcac72ec2e1fb
Min Xu <min.mxu@gmail.com>
2026-04-13 14:05:23 -0400

    update the tests for generic_netcdf.py

--- tests/unit/test_generic_netcdf.py
+++ tests/unit/test_generic_netcdf.py
@@ -7,28 +7,6 @@ from esgcet.generic_netcdf import GenericPublisher
 
 import pathlib
 
-@pytest.fixture
-def test_map_cmip6(data_dir):
-    test_map = (
-        data_dir
-        / "CMIP6/CMIP6.DCPP.MRI.MRI-ESM2-0.dcppA-hindcast.s2017-r1i1p1f1.Amon.psl.gn.v20210114.map"
-    )
-
-    return test_map
-
-@pytest.fixture
-def test_map_cmip7(data_dir):
-    test_map = (
-        data_dir
-       #/ "MIP-DRS7/MIP-DRS7.CMIP7.CMIP.CCCma.CanESM6-MR.historical.r2i1p1f3.glb.mon.pr.tavg-u-hxy-u.g99.v20260114.map"
-       / "MIP-DRS7/MIP-DRS7.CMIP7.CMIP.MOHC.UKESM1-0-LL.1pctCO2.r1i1p1f3.glb.mon.tas.tavg-h2m-hxy-u.g99.v20260123.map"
-       #/ "MIP-DRS7/MIP-DRS7.CMIP7.CMIP.MOHC.UKESM1-0-LL.1pctCO2.r1i1p1f3.glb.mon.pr.tavg-u-hxy-u.g99.v20260123.map"
-    )
-
-    return test_map
-
-
-
 
 def test_mapfile_get_args(test_map_cmip6):
     pub_args = PublisherArgs()
