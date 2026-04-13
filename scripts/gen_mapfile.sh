1b5d6e432431d7a9125c5ed992a87634562a9374
Min Xu <min.mxu@gmail.com>
2026-04-13 13:51:42 -0400

    add a bash script to generate map files

--- scripts/gen_mapfile.sh
+++ scripts/gen_mapfile.sh
@@ -1,27 +1,43 @@
 #!/usr/bin/env bash
 
 
-ROOT_DIR=/autofs/nccsopen-svm1_home/mfx/MyGit/esg-publisher/tests/unit/data
+ROOT_DIR=/nl/themis/esgf/cli137/mfx/Staged_E3SM/css03_data/
 
-find "$ROOT_DIR/MIP-DRS7/" -type f -name "*.nc" | sort | while read -r FILE; do
+MIP_ERA="CMIP6"  # "MIP-DRS7"
+
+FILTER_TAB='mon'
+
+find "$ROOT_DIR/$MIP_ERA/" -type f -name "*.nc" | sort | while read -r FILE; do
     # File metadata
     echo $FILE
-    SIZE=$(stat -c"%s" "$FILE")
-    MTIME=$(stat -c"%Z" "$FILE")
-
-    CHECKSUM=$(sha256sum "$FILE" | awk '{print $1}')
-    echo $SIZE, $MTIME, $CHECKSUM
 
     ## Extract CMIP6 components from path
     REL_PATH=${FILE#"$ROOT_DIR"/}
 
-    IFS='/' read -r DRS MIP_ERA MIP INST MODEL EXP MEMBER REGION TABLE VAR_ID BRANDING_SFX GRID VERSION FILENAME <<< "$REL_PATH"
+    if [[ $MIP_ERA == "MIP-DRS7" ]]; then
+        IFS='/' read -r DRS MIP_ERA MIP INST MODEL EXP MEMBER REGION TABLE VAR_ID BRANDING_SFX GRID VERSION FILENAME <<< "$REL_PATH"
+        ## Remove leading "v" from version
+        VERSION_CLEAN=${VERSION#v}
+        DATASET_ID="${DRS}.${MIP_ERA}.${MIP}.${INST}.${MODEL}.${EXP}.${MEMBER}.${REGION}.${TABLE}.${VAR_ID}.${BRANDING_SFX}.${GRID}.${VERSION}"
+        MAPFILE=$DATASET_ID
+    elif [[ $MIP_ERA == "CMIP6" ]]; then
+        IFS='/' read -r MIP_ERA MIP INST MODEL EXP MEMBER TABLE VAR_ID GRID VERSION FILENAME <<< "$REL_PATH"
+        VERSION_CLEAN=${VERSION#v}
+        DATASET_ID="${MIP_ERA}.${MIP}.${INST}.${MODEL}.${EXP}.${MEMBER}.${TABLE}.${VAR_ID}.${GRID}#${VERSION_CLEAN}"
+        MAPFILE="${MIP_ERA}.${MIP}.${INST}.${MODEL}.${EXP}.${MEMBER}.${TABLE}.${VAR_ID}.${GRID}.${VERSION}"
+    fi
 
-    ## Remove leading "v" from version
-    VERSION_CLEAN=${VERSION#v}
+    if [[ "$TABLE" == *"$FILTER_TAB" ]]; then
+        echo "processing $FILETR_TAB"
+    else
+        continue
+    fi
 
-    DATASET_ID="${DRS}.${MIP_ERA}.${MIP}.${INST}.${MODEL}.${EXP}.${MEMBER}.${REGION}.${TABLE}.${VAR_ID}.${BRANDING_SFX}.${GRID}.${VERSION}"
+    SIZE=$(stat -c"%s" "$FILE")
+    MTIME=$(stat -c"%Z" "$FILE")
+    CHECKSUM=$(sha256sum "$FILE" | awk '{print $1}')
+    echo $SIZE, $MTIME, $CHECKSUM
 
     FILE4TEST=${FILE/"$ROOT_DIR"/"\$TEST_DATA"}
-    echo "${DATASET_ID} | ${FILE4TEST} | ${SIZE} | mod_time=${MTIME}.0 | checksum=${CHECKSUM} | checksum_type=SHA256" > "$ROOT_DIR/MIP-DRS7/${DATASET_ID}.map"
+    echo "${DATASET_ID} | ${FILE} | ${SIZE} | mod_time=${MTIME}.0 | checksum=${CHECKSUM} | checksum_type=SHA256" >> "${MAPFILE}.map"
 done
