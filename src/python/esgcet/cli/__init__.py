aa359cc53d7487035f519dbd7ed160edb3956d02
Min Xu <min.mxu@gmail.com>
2026-09-01 12:15:18 -0400

    add descriptions for the esgcet subcommands

--- src/python/esgcet/cli/__init__.py
+++ src/python/esgcet/cli/__init__.py
@@ -10,6 +10,23 @@ app = typer.Typer(name="esgcet", no_args_is_help=True)
 
 # Add subcommands from other modules
 app.add_typer(kerchunk.app, name="kerchunk")
-app.add_typer(publisher.app, name="publish")
 app.add_typer(qc_check.app, name="qc-check")
 app.add_typer(migrate2stac.app, name="migrate2stac")
+
+app.add_typer(
+    kerchunk.app,
+    name="kerchunk",
+    help="Generate Kerchunks from a map file or a directory with multiple map files.",
+)
+
+app.add_typer(
+    qc_check.app,
+    name="qc-check",
+    help="Run quality-control checks on datasets.",
+)
+
+app.add_typer(
+    migrate2stac.app,
+    name="migrate2stac",
+    help="Migrate ESGF1.5 metadata to STAC.",
+)
